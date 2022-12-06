355814c6a225904de4f06cefb5cf12099a3aa4ec
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2022-12-07 00:29:02 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <assert.h>
 
-//Method to test the printColorMap function
 void formatPrintfInputColorMap (int majorNumberIndex, int minorNumberIndex, const char* recvMajorColor, const char* recvMinorColor)
 {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     bool isSingleDigit = true;
     
-    //Test case to check for proper major and minor colors
     assert(majorColor[majorNumberIndex] == recvMajorColor);
     assert(minorColor[minorNumberIndex] == recvMinorColor);
     
     isSingleDigit = (majorNumberIndex * minorNumberIndex) / 10;
     
-    //Test case to test single digit and it fails as alignment is not right
     assert(isSingleDigit == 1);
 }
 
