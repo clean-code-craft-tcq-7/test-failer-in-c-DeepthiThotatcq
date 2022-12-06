abb7f6ccaf6f51b74275f479258bd0af083706bc
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2022-12-07 00:03:54 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,45 +1,49 @@
 #include <stdio.h>
 #include <assert.h>
-#include <string.h> 
 
-const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+int alertFailureCount = 0;
 
-typedef struct{
-    int colorCode;
-    const char *majorColor;
-    const char *minorColor;
-}colorPair;
-
-colorPair getColorCode(int i, int j){
-    colorPair colorPair;
-    colorPair.colorCode = i * 5 + j;
-    colorPair.majorColor = majorColor[i];
-    colorPair.minorColor = minorColor[i];
-    return colorPair;
-}
-int printColorMap() {
-    int i = 0, j = 0;
-    colorPair colorPair;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            colorPair = getColorCode(i, j);
-            printf("%d | %s | %s\n", colorPair.colorCode, colorPair.majorColor, colorPair.minorColor);
-        }
-    }
-    return i * j;
+int networkAlertStub(float celcius) {
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+    if (celcius > 1000 && celcius < 0)
+       return 500;
+     else 
+       return 200;
 }
 
+int networkAlert(float celcius) {
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    return 0;
+}
+
+
+void alertInCelcius(float farenheit, int (*networkerAlert)(float)) {
+    float celcius = (farenheit - 32) * 5 / 9;
+    int returnCode = networkerAlert(celcius);
+
+    if (returnCode != 200) {
+        // non-ok response is not an error! Issues happen in life!
+        // let us keep a count of failures to report
+        // However, this code doesn't count failures!
+        // Add a test below to catch this bug. Alter the stub above, if needed.
+        alertFailureCount += 1;
+     }
+}
 
 int main() {
-    colorPair colorPair;
-    int result = printColorMap();
-    assert(result == 25);
+    int (*networkerAlert)(float) = &networkAlertStub;
+    alertInCelcius(400.5, networkerAlert);
+    assert(alertFailureCount == 0);
+    alertInCelcius(303.6, networkerAlert);
+    assert(alertFailureCount == 0);
+    alertInCelcius(1900.0, networkerAlert);
+    assert(alertFailureCount == 1);
+    alertInCelcius(0, networkerAlert);
+    assert(alertFailureCount == 2);
+    printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
-    colorPair = getColorCode(0, 1);
-    assert(colorPair.colorCode == 1);
-    assert(strcmp(colorPair.majorColor, majorColor[0]) == 0);
-    assert(strcmp(colorPair.minorColor, minorColor[1]) == 0);
-
     return 0;
 }
